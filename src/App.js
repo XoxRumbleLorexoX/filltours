7ba5f551cfbfb23c2204eb1bd3dd52e4553bff35
XoxRumbleLorexoX <shan.lingeswaran@hotmail.co.uk>
2023-06-03 14:50:53 +0100

    Signs in to the webapp & protects the api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React, { useState } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+// Initialize Firebase
+const firebaseConfig = {
+  // Your Firebase configuration
+  apiKey: process.env.API_KEY,
+  authDomain: 'filltours.firebaseapp.com',
+  projectId: 'filltours',
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Add an observer to handle user authentication state changes
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    // Clean up the observer on unmount
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleSignIn = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.error('Error signing in:', error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
+
   const [selectedFile, setSelectedFile] = useState();
 
   const handleFileChange = (event) => {
@@ -33,12 +78,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to my app!</h1>
-        <p>Upload your image or video to get started.</p>
-        <input type="file" accept="image/*,video/*" onChange={handleFileChange} />
-        <button onClick={handleUpload}>Upload</button>
+        {user ? (
+          <div>
+            <p>Upload your image or video to get started.</p>
+            <input type="file" accept="image/*,video/*" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+            <button onClick={handleSignOut}>Sign Out</button>
+          </div>
+        ) : (
+          <button onClick={handleSignIn}>Sign In with Google</button>
+        )}
       </header>
     </div>
   );
-}
+        }
 
 export default App;
